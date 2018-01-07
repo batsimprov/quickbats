7098ae7841aead5cf3fec1dc0c50ada6f60df53b
Ana Nelson <ana@cosmify.io>
2018-01-07 13:54:30 -0800

    Add config/auth example files.

---
+++ quickbats/tokens-example.py
@@ -0,0 +1,7 @@
+TOKENS = {
+        "refresh_token": "",
+        "access_token": "",
+        "expires_in": 3600,
+        "x_refresh_token_expires_in": 99999,
+        "token_type": "bearer"
+        }
